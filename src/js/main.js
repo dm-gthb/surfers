449b1a8c7fe3c04300bbb5750d0c7e413c69d982
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2018-02-24 19:26:53 +0300

    add styles, markup fixes

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var title = document.querySelector('.boards__title');
+var boardSection = document.querySelector('.boards');
+var minWidth = 768;
+var viewportWidth = document.body.offsetWidth;
+
+
+var getBoardSectionHeight = function() {
+    var titleWidth = title.offsetWidth;
+    var boardSectionHeight = boardSection.offsetHeight;
+
+    if (viewportWidth >= minWidth && boardSectionHeight < titleWidth) {
+      boardSection.style.height = titleWidth + 'px';
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    getBoardSectionHeight();
+});
+
+
+
+// если about__description будет больше чем ....
+// (так что about станет ощщутимо больше чем 640px высоты)
+// то
+
+// устанавливаем для about фиксированную высоту (например 640) и увечиваем margin-bottom
